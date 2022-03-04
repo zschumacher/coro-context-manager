a09fa8b58fb98bf43f628031d7fb7c1473606f5a
Zach Schumacher <zachary@simplebet.io>
2022-03-04 17:15:30 -0500

    fix CI

--- coro_context_manager/__init__.py
+++ coro_context_manager/__init__.py
@@ -31,14 +31,14 @@ class CoroContextManager(Coroutine[Any, Any, _TObj], Generic[_TObj]):
         typ: Type[BaseException],
         val: Optional[Union[BaseException, object]] = None,
         tb: Optional[TracebackType] = None,
-    ) -> Any: # pragma: no cover
+    ) -> Any:  # pragma: no cover
         if val is None:
             return self._coro.throw(typ)
         if tb is None:
             return self._coro.throw(typ, val)
         return self._coro.throw(typ, val, tb)
 
-    def close(self) -> None: # pragma: no cover
+    def close(self) -> None:  # pragma: no cover
         self._coro.close()
 
     def __await__(self) -> Generator[Any, None, _TObj]:  # pragma: no cover
@@ -48,10 +48,10 @@ class CoroContextManager(Coroutine[Any, Any, _TObj], Generic[_TObj]):
         if self._obj is None:  # pragma: no branch
             self._obj = await self._coro
         if hasattr(self._obj, "__aenter__"):  # pragma: no branch
-            await self._obj.__aenter__() # type: ignore
+            await self._obj.__aenter__()  # type: ignore
         assert self._obj
         return self._obj
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        if hasattr(self._obj, "__aexit__"): # pragma: no branch
-            await self._obj.__aexit__(exc_type, exc_val, exc_tb) # type: ignore
+        if hasattr(self._obj, "__aexit__"):  # pragma: no branch
+            await self._obj.__aexit__(exc_type, exc_val, exc_tb)  # type: ignore
